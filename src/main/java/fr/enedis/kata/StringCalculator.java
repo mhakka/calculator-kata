1cbf50f839306b672152e04c7e8b1a73c18782c8
HAKKA Mehdi <MH7C4F5N@2KZE0.ZE0.ERD.EDF.FR>
2020-10-07 00:22:45 +0200

    Etape 1

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -3,6 +3,12 @@ package fr.enedis.kata;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return -1;
+        if ("".equals(numbers)) return 0;
+        String[] numbersTab = numbers.split(",");
+        if (numbersTab.length == 2) {
+            return Integer.parseInt(numbersTab[0])
+                    + Integer.parseInt(numbersTab[1]);
+        }
+        return Integer.parseInt(numbersTab[0]);
     }
 }
--- src/test/java/fr/enedis/kata/StringCalculatorTest.java
+++ src/test/java/fr/enedis/kata/StringCalculatorTest.java
@@ -1,7 +1,58 @@
 package fr.enedis.kata;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
-    // Right tests here
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void should_return_0_if_string_is_empty () {
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+
+    @Test
+    public void should_return_1_if_string_is_1 () {
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void should_return_2_if_string_is_2 () {
+        assertEquals(2, stringCalculator.add("2"));
+    }
+
+    @Test
+    public void should_return_3_if_string_is_1comma2 () {
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void should_return_13_if_string_is_13 () {
+        assertEquals(13, stringCalculator.add("13"));
+    }
+
+    @Test
+    public void should_return_19_if_string_is_12comma7 () {
+        assertEquals(19, stringCalculator.add("12,7"));
+    }
+
+    @Test
+    public void should_return_19_if_string_is_4comma15 () {
+        assertEquals(19, stringCalculator.add("4,15"));
+    }
+
+    @Test
+    public void should_return_110_if_string_is_35comma75 () {
+        assertEquals(110, stringCalculator.add("35,75"));
+    }
 
 }
