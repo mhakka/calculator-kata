4e072f0546cadec522c73eaaf5038e3b14101a9f
MESSAOUI <mohand-ameziane.messaoui@etu.u-pec.fr>
2020-10-07 12:03:45 +0200

    etape1

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -2,6 +2,21 @@ package fr.enedis.kata;
 
 public class StringCalculator {
 
+    public static int ajouter(String s) {
+        if(s == ""){
+            return 0;
+        }else{
+            if(s.length() == 1){
+                return Integer.parseInt(s);
+            }else{
+                String[] result = s.split(",");
+                return (Integer.parseInt(result[0]) + Integer.parseInt(result[1]));
+            }
+        }
+
+
+    }
+
     public int add(String numbers) {
         return -1;
     }
--- src/test/java/fr/enedis/kata/StringCalculatorTest.java
+++ src/test/java/fr/enedis/kata/StringCalculatorTest.java
@@ -1,6 +1,31 @@
 package fr.enedis.kata;
 
+import org.junit.Test;
+import static  org.junit.Assert.*;
+
 public class StringCalculatorTest {
+
+
+@Test
+ public void testAjouter(){
+    //given
+    String vide = "";
+    String one = "1";
+    String tow = "1,2";
+
+    //when
+    int ajoutVide = StringCalculator.ajouter("");
+    int ajoutOne = StringCalculator.ajouter("1");
+    int ajoutTow = StringCalculator.ajouter("1,2");
+
+    //then
+
+    assertEquals(0, ajoutVide);
+    assertEquals(1, ajoutOne);
+    assertEquals(3, ajoutTow);
+
+
+}
 
     // Right tests here
 
