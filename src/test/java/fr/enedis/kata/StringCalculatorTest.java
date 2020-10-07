2b27fde0ce28edce2cbd02bb2b530c38a444c8f2
hicham <hicham.ouhhabi@gmail.com>
2020-10-07 11:49:14 +0200

    case return 0

--- src/test/java/fr/enedis/kata/StringCalculatorTest.java
+++ src/test/java/fr/enedis/kata/StringCalculatorTest.java
@@ -1,7 +1,22 @@
 package fr.enedis.kata;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
     // Right tests here
 
+
+    @Test
+    public void whenStringIsEmpty(){
+
+        //given
+        StringCalculator stringCalculator = new StringCalculator();
+
+        //when
+        int res = stringCalculator.add("");
+        //then
+        Assert.assertEquals(0,res);
+    }
 }
