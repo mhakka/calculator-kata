fce2420130bdde5c227ca90e531fa808d7310db7
brahim <bnaitdjoudi@gmail.com>
2020-10-07 12:02:57 +0200

    etape 1

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -3,6 +3,19 @@ package fr.enedis.kata;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return -1;
+        if("".equals(numbers)) {
+            return 0;
+        }else{
+            int somme = 0;
+            String [] nums = numbers.split(",");
+
+            for(String num:nums){
+
+                somme+=Integer.parseInt(num);
+
+            }
+            return somme;
+        }
+
     }
 }
--- src/test/java/fr/enedis/kata/StringCalculatorTest.java
+++ src/test/java/fr/enedis/kata/StringCalculatorTest.java
@@ -1,7 +1,19 @@
 package fr.enedis.kata;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
     // Right tests here
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        StringCalculator stringCalculator = new StringCalculator();
+
+        assertEquals(0, stringCalculator.add(""), "\"\" must be equals to 0");
+        assertEquals(1, stringCalculator.add("1"), "\"1\" must be equals to 1");
+        assertEquals(3, stringCalculator.add("1,2"), "\"1,2\" must be equals to 3");
+    }
 
 }
