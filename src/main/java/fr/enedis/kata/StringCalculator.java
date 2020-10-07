568efc4fa5fa4853bf8d74a118bd618d25499570
houhhabi <51961297+houhhabi@users.noreply.github.com>
2020-10-07 19:17:46 +0200

    Update StringCalculator.java
    Using split operator

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -3,11 +3,15 @@ package fr.enedis.kata;
 public class StringCalculator {
 
     public int add(String numbers) {
-        switch (numbers) {
-            case "" : return 0;
-            case "1" : return 1;
-            case "1.2" : return 3;
-            default : return -1;
+        if("".equals(numbers)) return 0;
+
+        String[] nums = numbers.split(",");
+
+        int sum = 0;
+
+        for(String num : nums){
+            sum+=Integer.parseInt(num);
         }
+        return sum;
     }
 }
