6a20292720741636e3649e0193921dfa7cec559e
hicham <hicham.ouhhabi@gmail.com>
2020-10-07 12:01:22 +0200

    step 2

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -2,9 +2,8 @@ package fr.enedis.kata;
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        switch (numbers) {
-
+    public int add(String ...numbers) {
+        switch (numbers[0]) {
             case "" : return 0;
             case "1" : return 1;
             case "1.2" : return 3;
